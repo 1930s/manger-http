0ac7fc0f4473b081f17854ea1e9601d403cacd73
Michael Nisi <michael.nisi@gmail.com>
2017-12-05 10:50:31 +0100

    Complete feed test

--- test/lib/fixtures.js
+++ test/lib/fixtures.js
@@ -2,7 +2,6 @@ const common = require('./common')
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
-const stream = require('stream')
 const url = require('url')
 
 exports.run = run
@@ -23,30 +22,54 @@ function readSync (file) {
   return mkArray(json)
 }
 
+// Returns a fake remote server for manger-http to hit.
 function freshRemoteServer (fixture, t) {
-  const r = mkArray(fixture.remote)
-  if (!r) return
+  const remotes = fixture.reduce((acc, f) => {
+    const r = mkArray(f.remote)
+    if (r instanceof Array) return acc.concat(r)
+    return acc
+  }, [])
 
-  const fixtures = r.map((f) => {
+  if (remotes.length == 0) return
+
+  const fixtures = remotes.map((f) => {
     return (req, res) => {
-      t.is(req.url, f.url)
-      res.writeHead(f.statusCode || 404, f.head)
+      t.is(req.url, f.url, 'should be URL')
+
+      // If the fixture does not provide a status code, we’re assuming an error,
+      // thus we’re bailing out with internal server error, 500.
+      if (!f.statusCode) {
+        res.writeHead(500)
+        res.end()
+        return
+      }
+
+      res.writeHead(f.statusCode, f.head)
 
       if (f.file) {
         const p = resolve(f.file)
-        stream.createReadStream(p).pipe(res)
+        t.comment(`streaming: ${p}`)
+        fs.createReadStream(p).pipe(res)
       } else {
         res.end(f.payload)
       }
     }
   })
 
-  return http.createServer((req, res) => {
+  const server = http.createServer((req, res) => {
     const f = fixtures.shift()
     f(req, res)
   })
+
+  server.stop = (cb) => {
+    t.is(fixtures.length, 0)
+    server.close(cb)
+  }
+
+  return server
 }
 
+// Returns request options for our test target, the local manger-http API.
 function httpRequestOpts (json) {
   let opts = Object.assign(Object.create(null), json)
   opts.port = opts.port || 1337
@@ -76,7 +99,7 @@ function test (server, fixtures, t, cb) {
     res.on('end', () => {
       const found = JSON.parse(acc)
       const body = wanted.payload
-      t.same(found, body, 'should be payload')
+      t.matches(found, body, 'should match payload')
       test(server, fixtures, t, cb)
     })
     res.resume()
@@ -89,6 +112,8 @@ function run (file, t, cb) {
   const fixtures = readSync(file)
   const server = common.freshMangerServer()
 
+  const remoteServer = freshRemoteServer(fixtures, t)
+
   function go () {
     server.start((er) => {
       if (er) return cb(er)
@@ -97,7 +122,7 @@ function run (file, t, cb) {
         server.stop((er) => {
           if (er) throw er
           if (remoteServer) {
-            remoteServer.close((er) => {
+            remoteServer.stop((er) => {
               cb(er)
             })
           } else {
@@ -108,10 +133,8 @@ function run (file, t, cb) {
     })
   }
 
-  const remoteServer = freshRemoteServer(fixtures, t)
-
   if (remoteServer) {
-    remoteServer.listen(1338, (er) => {
+    remoteServer.listen(8001, (er) => {
       if (er) throw er
       go()
     })
